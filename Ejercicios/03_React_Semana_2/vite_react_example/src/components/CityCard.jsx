4d90a920582f6796689dec5a3cde55bf8019b70f
Camila Arias <ariasruizcamilaa@gmail.com>
2024-08-27 18:09:30 -0500

    change || to ??

--- Ejercicios/03_React_Semana_2/vite_react_example/src/components/CityCard.jsx
+++ Ejercicios/03_React_Semana_2/vite_react_example/src/components/CityCard.jsx
@@ -5,14 +5,15 @@ export const Card = ({ ciudad }) => {
     return null; // No renderiza nada si no hay ciudad seleccionada
   }
 
+
   return (
     <div className="card-city">
       <h2>{ciudad.name}</h2>
-      <p>{ciudad.description || "No hay descripción disponible"}</p>
+      <p>{ciudad.description ?? "No hay descripción disponible"}</p>
       <ul>
         <li>Superficie: {ciudad.surface ? `${ciudad.surface} km²` : "No disponible"}</li>
-        <li>Población: {ciudad.population || "No disponible"}</li>
-        <li>Código Postal: {ciudad.postalCode || "No disponible"}</li>
+        <li>Población: {ciudad.population ?? "No disponible"}</li>
+        <li>Código Postal: {ciudad.postalCode ?? "No disponible"}</li>
       </ul>
     </div>
   );
